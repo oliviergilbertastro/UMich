16759dd83a7dee6b0883f6746c776a65c5b16d39
Olivier Desktop <ogilbert@umich.edu>
2026-01-26 13:24:32 -0500

    mesa

---
+++ ASTRO531/hw2/hw2_6.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['xtick.labelsize'] = 16
+mpl.rcParams['ytick.labelsize'] = 16
+plt.rc("axes", labelsize=16) 
+import astropy.units as u
+from astropy.constants import c, L_sun
+import mesa_web as m
+
+m.read_history()
+
