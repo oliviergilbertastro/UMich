c89cdc847c2d49443d14272824d7d6f00b3285b8
Olivier Desktop <ogilbert@umich.edu>
2026-01-26 14:59:56 -0500

    profile number

--- ASTRO531/hw2/hw2_6.py
+++ ASTRO531/hw2/hw2_6.py
@@ -9,21 +9,22 @@ from astropy.constants import c, L_sun
 import mesa_web as m
 from tqdm import tqdm
 
-hist = m.read_history("", as_table=True)
-table = m.read_history(r"ASTRO531\hw2\MESA-Web_Job_01252661968\profile1.data", as_table=True)
-print(table.columns)
-
+hist = m.read_history(r"ASTRO531\hw2\MESA-Web_Job_01252661968\trimmed_history.data", as_table=True)
+print(hist)
+star_age = list(hist["star_age"])
+model_number = list(hist["model_number"])
+# Find model closest to 4.5Gyr
+star_age_delta = list(np.abs(np.array(star_age)-4.5E9))
+idx = star_age_delta.index(min(star_age_delta))
+# Model to use along with the stellar age it corresponds to:
+print(model_number[idx], star_age[idx])
+# The closest model number to 294 in profiles.index is 296, so we use profile 8.
 
 # a) Verify Virial theorem:
-Us = []
-for i in tqdm(range(1,290)):
-    try:
-        table_i = m.read_history(r"ASTRO531\hw2\MESA-Web_Job_01252661968\profile"+f"{i}"+".data", as_table=True)
-        E_int = table_i["energy"] # as a cumulative function of radius
-        U = E_int[-1]
-        Us.append(U)
-    except:
-        pass
 
-plt.plot(Us)
+table = m.read_profile(r"ASTRO531\hw2\MESA-Web_Job_01252661968\profile8.data", as_table=True)
+E_int = table["energy"] # as a cumulative function of radius  
+U = E_int[-1]
+print(U)
+plt.plot(U)
 plt.show()
