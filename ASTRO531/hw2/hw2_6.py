197f3a12e6e13a18c74e6641bbe1f10ff6f066ae
olivier_UM_mac <ogilbert@umich.edu>
2026-01-26 16:06:13 -0500

    #6

--- ASTRO531/hw2/hw2_6.py
+++ ASTRO531/hw2/hw2_6.py
@@ -9,7 +9,10 @@ from astropy.constants import c, L_sun
 import mesa_web as m
 from tqdm import tqdm
 
-hist = m.read_history(r"ASTRO531\hw2\MESA-Web_Job_01252661968\trimmed_history.data", as_table=True)
+try:
+    hist = m.read_history(r"ASTRO531\hw2\MESA-Web_Job_01252661968\trimmed_history.data", as_table=True)
+except:
+    hist = m.read_history(r"ASTRO531/hw2/MESA-Web_Job_01252661968/trimmed_history.data", as_table=True)
 print(hist)
 star_age = list(hist["star_age"])
 model_number = list(hist["model_number"])
@@ -22,9 +25,24 @@ print(model_number[idx], star_age[idx])
 
 # a) Verify Virial theorem:
 
-table = m.read_profile(r"ASTRO531\hw2\MESA-Web_Job_01252661968\profile8.data", as_table=True)
+
+try:
+    table = m.read_profile(r"ASTRO531\hw2\MESA-Web_Job_01252661968\profile8.data", as_table=True)
+except:
+    table = m.read_profile(r"ASTRO531/hw2/MESA-Web_Job_01252661968/profile8.data", as_table=True)
+print(table.columns)
 E_int = table["energy"] # as a cumulative function of radius  
 U = E_int[-1]
+E_grav = 0
+
 print(U)
-plt.plot(U)
+plt.plot(table["mass"])
+plt.plot(table["radius"])
 plt.show()
+
+plt.plot(table["radius"], table["mass"])
+plt.xlabel(r"Radius [$R_\odot$]")
+plt.ylabel(r"Mass [$M_\odot$]")
+plt.tight_layout()
+plt.show()
+
