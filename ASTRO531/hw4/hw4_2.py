c90395f7fea08df80c2fc8b9538cf4e3e7410103
Olivier Desktop <ogilbert@umich.edu>
2026-02-09 21:31:35 -0500

    hw4

--- ASTRO531/hw4/hw4_2.py
+++ ASTRO531/hw4/hw4_2.py
@@ -1,3 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy.table import Table
 
+t = Table.read(
+    "table7.dat",
+    format="ascii.fixed_width",
+    header_start=0,
+    data_start=4
+)
