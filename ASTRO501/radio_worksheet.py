d4c6b9c1197e6f1efe77d2b4c732cae460c0f2e0
olivier_UM_mac <ogilbert@umich.edu>
2026-03-10 09:59:32 -0400

    radio worksheet 2

--- ASTRO501/radio_worksheet.py
+++ ASTRO501/radio_worksheet.py
@@ -1,7 +1,10 @@
 import astropy.units as u
 import astropy.constants as cst
-
+import numpy as np
 
 T_B = 500*u.m**2 * 1*u.Jy / cst.k_B
 
+print(T_B.to(u.K))
+
+T_B = np.pi*u.m**2 * 1*u.Jy / cst.k_B
 print(T_B.to(u.K))
