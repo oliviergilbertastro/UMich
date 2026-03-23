10e8137e9b9f5a2d2ef43bda751b8b450530e9fb
olivier_UM_mac <ogilbert@umich.edu>
2026-03-23 18:26:20 -0400

    5bc done

--- ASTRO501/hw3a/5b.py
+++ ASTRO501/hw3a/5b.py
@@ -6,6 +6,7 @@ mpl.rcParams['ytick.labelsize'] = 16
 plt.rc("axes", labelsize=16) 
 
 phi = np.deg2rad(40)
+dec = np.deg2rad(0)
 HA = np.linspace(-6, 6, 200) * 15 * np.pi/180  # radians
 
 baselines = [(1,0.5),(5,2),(4,1.5),(5.5,1),(0.5,-1),(1.5,-0.5)] # east, north
@@ -14,7 +15,7 @@ for i,B in enumerate(baselines):
     Bn = B[1]    # north component
 
     u = Be*np.cos(HA) - Bn*np.sin(phi)*np.sin(HA)
-    v = Be*np.sin(HA) + Bn*np.sin(phi)*np.cos(HA)
+    v = Be*np.sin(HA)*np.sin(dec) + Bn*(np.sin(phi)*np.cos(HA)*np.sin(dec)+np.cos(dec)*np.cos(phi))
 
     plt.plot(u, v, label=f"Baseline {i}")
 
@@ -22,4 +23,6 @@ plt.xlabel('u')
 plt.ylabel('v')
 plt.axis('equal')
 plt.legend(fontsize=12)
+plt.tight_layout()
+plt.savefig(r"ASTRO501/hw3a/5c.pdf")
 plt.show()
