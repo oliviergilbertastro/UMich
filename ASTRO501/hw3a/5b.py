137cfb461dd4465602b3cf7a58700a0a873d76fb
olivier_UM_mac <ogilbert@umich.edu>
2026-03-23 18:20:56 -0400

    5b

--- ASTRO501/hw3a/5b.py
+++ ASTRO501/hw3a/5b.py
@@ -8,19 +8,18 @@ plt.rc("axes", labelsize=16)
 phi = np.deg2rad(40)
 HA = np.linspace(-6, 6, 200) * 15 * np.pi/180  # radians
 
-# example baseline (replace with real ones)
-baselines = [()] # east, north
-Be = 100   # east component (m)
-Bn = 50    # north component (m)
+baselines = [(1,0.5),(5,2),(4,1.5),(5.5,1),(0.5,-1),(1.5,-0.5)] # east, north
+for i,B in enumerate(baselines):
+    Be = B[0]   # east component
+    Bn = B[1]    # north component
 
-u = Be*np.cos(HA) - Bn*np.sin(phi)*np.sin(HA)
-v = Be*np.sin(HA) + Bn*np.sin(phi)*np.cos(HA)
+    u = Be*np.cos(HA) - Bn*np.sin(phi)*np.sin(HA)
+    v = Be*np.sin(HA) + Bn*np.sin(phi)*np.cos(HA)
 
-plt.plot(u, v)
-
-# repeat for all baselines...
+    plt.plot(u, v, label=f"Baseline {i}")
 
 plt.xlabel('u')
 plt.ylabel('v')
 plt.axis('equal')
+plt.legend(fontsize=12)
 plt.show()
