217a5fdb5155dd1c83e457300265df8de809f2f1
Olivier Desktop <ogilbert@umich.edu>
2026-04-11 12:20:32 -0400

    gradients 1b

--- ASTRO531/hw8/1b.py
+++ ASTRO531/hw8/1b.py
@@ -108,3 +108,60 @@ plt.title(r"5$M_\odot$",fontsize=16)
 plt.tight_layout()
 plt.savefig("ASTRO531/hw8/figures/1b_5_ionization.pdf")
 plt.close()
+
+
+
+
+
+
+
+
+
+def plot_gradients(table):
+    rmo = table["opacity"]*(u.cm**2/u.g)
+    grad_a = table["grada"]
+    grad_R = table["gradr"]
+    grad_T = table["gradT"]
+    logT = (table["logT"])
+    R = table["radius"]
+
+    from scipy.interpolate import interp1d
+    x_primary = np.array(logT)
+    x_top_vals = np.array(R)
+    # your data
+
+
+    plt.figure(figsize=(8,6))
+
+    ax2 = plt.subplot(111)
+    ax2.plot(logT, grad_a, ls="-", lw=2, color="red", label=r"$\nabla_a$")
+    ax2.plot(logT, grad_R, ls="-", lw=2, color="blue", label=r"$\nabla_R$")
+    ax2.plot(logT, grad_T, ls="-", lw=2, color="green", label=r"$\nabla_T$")
+    ax2.legend(fontsize=15)
+    ax2.invert_xaxis()
+    ax2.set_xlabel(r"$\log T$ [$\mathrm{K}$]")
+    ax2.set_ylabel(r"Gradient")
+    # create top axis
+    secax = ax2.twiny()
+    secax.set_xlim(ax2.get_xlim())
+
+    # choose clean points
+    xticks = ax2.get_xticks()
+    logT_arr = np.array(logT)
+    inds = [np.argmin(np.abs(logT_arr - xt)) for xt in xticks]
+    inds = np.unique(inds)  # avoid duplicates
+    inds = inds[1:-1]
+    print(len(x_primary))
+    print(inds)
+    print(x_primary[inds])
+    secax.set_xticks(x_primary[inds])
+    secax.set_xticklabels([f"{x_top_vals[i]:.2f}" for i in inds])
+    secax.set_xlabel(r"Radius [$R_\odot$]")
+    plt.ylim(0, 1.2)
+    plt.tight_layout()
+
+plot_gradients(profile_05_Msun)
+plt.title(r"0.5$M_\odot$",fontsize=16)
+plt.tight_layout()
+plt.savefig("ASTRO531/hw8/figures/1b_05_gradients.pdf")
+plt.show()
