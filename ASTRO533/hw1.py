fd8099434f595a587ee0328b0bb55cd58c7e90db
Olivier Desktop <ogilbert@umich.edu>
2026-09-10 21:17:44 -0400

    does this work?

--- ASTRO533/hw1.py
+++ ASTRO533/hw1.py
@@ -15,3 +15,8 @@ print(m_H2)
 
 m_cloud = N_H2*m_H2
 print(m_cloud)
+
+m_all_clouds_in_MW = (1.5E9*(u.M_sun)).to(u.g)
+
+N_clouds_in_MW = m_all_clouds_in_MW/m_cloud
+print("N_clouds_in_MW:", N_clouds_in_MW)
