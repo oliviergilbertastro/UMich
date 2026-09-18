3c54d3f6158022377581c78bcc0fc6be73c28287
Olivier Desktop <ogilbert@umich.edu>
2026-09-18 02:05:41 -0400

    Q3 done

--- ASTRO533/hw2.py
+++ ASTRO533/hw2.py
@@ -9,3 +9,18 @@ print((cst.k_B).to(u.erg/u.K))
 
 lambda_peak = (cst.h).to(u.erg * u.s)*(cst.c).to(u.um/u.s)/(cst.k_B).to(u.erg/u.K)/2.8
 print(lambda_peak)
+
+
+lam = 1216.67*u.AA
+A_ul = 6E8/u.s
+
+FWHM = A_ul/(2*np.pi)
+FWHM_lam = FWHM*lam**2/cst.c
+print(FWHM_lam.to(u.AA))
+FWHM_kms = FWHM*lam
+print(FWHM_kms.to(u.km/u.s))
+
+b = 1.28*u.km/u.s * (6000/100)**(1/2)
+print(b)
+FWHM_lam = b*lam/cst.c
+print(FWHM_lam.to(u.AA))
